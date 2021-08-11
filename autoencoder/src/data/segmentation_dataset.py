646e6b20d0fd83b7b4574e44a40238b970ab9bf0
Ignacio de los Rios <ga84lew@mytum.de>
2021-08-11 13:28:07 +0200

    update dataloader

--- autoencoder/src/data/segmentation_dataset.py
+++ autoencoder/src/data/segmentation_dataset.py
@@ -14,12 +14,25 @@ import _pickle as pickle
 
 LABELS_LIST = [
     {"id": -1, "name":"void", "rgb_values": [0, 0, 0]},
-    {"id": 0, "name":"PIP", "rgb_values": [0, 0, 0]},
-    {"id": 1, "name":"MCP", "rgb_values": [0, 0, 0]},
-    {"id": 2, "name":"CMC", "rgb_values": [0, 0, 0]},
-    {"id": 3, "name":"Wrist", "rgb_values": [0, 0, 0]}
+    {"id": 0, "name":"PIP", "rgb_values": [36, 28, 236]},
+    {"id": 1, "name":"MCP", "rgb_values": [69, 209, 14]},
+    {"id": 2, "name":"CMC", "rgb_values": [183, 50, 250]},
+    {"id": 3, "name":"Wrist", "rgb_values": [10, 0, 0]}
 ]
 
+def label_img_to_rgb(label_img):
+    label_img = np.squeeze(label_img)
+    labels = np.unique(label_img)
+    label_infos = [l for l in LABELS_LIST if l['id'] in labels]
+
+    label_img_rgb = np.array([label_img,
+                              label_img,
+                              label_img]).transpose(1,2,0)
+    for l in label_infos:
+        mask = label_img == l['id']
+        label_img_rgb[mask] = l['rgb_values']
+
+    return label_img_rgb.astype(np.uint8)
 
 class Data(data.Dataset):
 
